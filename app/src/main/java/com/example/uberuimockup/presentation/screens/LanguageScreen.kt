7c667a5fc1ecc909548ed5e990cc167ed5aa90e3
TILLERN <brysonminodi@google.com>
2022-02-21 03:43:45 +0300

    lang btn done

--- app/src/main/java/com/example/uberuimockup/presentation/screens/LanguageScreen.kt
+++ app/src/main/java/com/example/uberuimockup/presentation/screens/LanguageScreen.kt
@@ -2,9 +2,11 @@ package com.example.uberuimockup.presentation.screens
 
 import android.widget.Space
 import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.selection.selectable
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -15,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
@@ -22,6 +25,7 @@ import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
+import com.example.uberuimockup.R
 import com.example.uberuimockup.presentation.components.HeaderIcon
 import com.example.uberuimockup.presentation.components.RadioButtons
 import com.example.uberuimockup.presentation.navigation.NavigationItem
@@ -29,13 +33,16 @@ import org.intellij.lang.annotations.JdkConstants
 
 @Composable
 fun LanguageScreen(navController: NavController) {
-    Column() {
+    Column(modifier=Modifier
+        .padding(5.dp)
+        .fillMaxSize()) {
 
 
-        Box() {
+        Box(modifier = Modifier.padding(start = 20.dp, end = 20.dp),
+            contentAlignment = Alignment.CenterStart) {
 
 
-            Column(modifier=Modifier.padding(5.dp)) {
+            Column(modifier = Modifier.fillMaxSize()) {
                 Spacer(modifier=Modifier.height(5.dp))
                 HeaderIcon()
                 Spacer(modifier=Modifier.height(3.dp))
@@ -55,84 +62,79 @@ fun LanguageScreen(navController: NavController) {
                 Divider()
                 Spacer(modifier=Modifier.height(5.dp))
 
-
-                    val radioOptions = listOf("English", "Japanese", "Swahili", "Latino", "Francais" ,"Germany")
-                    val (selectedOption, onOptionSelected) = remember { mutableStateOf(radioOptions[5]) }
-
-                    Column(
-                        modifier = Modifier.fillMaxSize(),
-                        verticalArrangement = Arrangement.Top,
-                        horizontalAlignment = Alignment.CenterHorizontally,
+                val radioOptions = listOf("English", "Japanese", "Swahili", "Latino", "Francais" ,"Germany")
+                val (selectedOption, onOptionSelected) = remember { mutableStateOf(radioOptions[5]) }
+                radioOptions.forEach { text ->
+                    Row(
+                        Modifier
+                            .fillMaxWidth()
+                            .selectable(
+                                selected=(text == selectedOption),
+                                onClick={ onOptionSelected(text) }
+                            )
+                            .padding(horizontal=16.dp)
                     ) {
-                        Spacer(modifier=Modifier.height(30.dp))
-                        Column {
-                            //set data to each radio button
-                            radioOptions.forEach { text ->
-                                Row(
-                                    Modifier
-                                        .fillMaxWidth()
-                                        .selectable(
-                                            selected=(text == selectedOption),
-                                            onClick={ onOptionSelected(text) }
-                                        )
-                                        .padding(horizontal=16.dp)
-                                ) {
-                                    Card() {
-                                        Row(
-                                            modifier = Modifier.fillMaxWidth(),
-                                            horizontalArrangement =Arrangement.SpaceBetween,
-                                            verticalAlignment =Alignment.CenterVertically) {
-                                            Text(
-                                                text = text,
-                                                modifier = Modifier.padding(start = 16.dp)
-                                            )
+                        Card() {
+                            Row(
+                                modifier = Modifier.fillMaxWidth(),
+                                horizontalArrangement =Arrangement.SpaceBetween,
+                                verticalAlignment =Alignment.CenterVertically) {
+                                Text(
+                                    text = text,
+                                    modifier = Modifier.padding(start = 16.dp)
+                                )
 
-                                            val context = LocalContext.current
+                                val context = LocalContext.current
 
-                                            RadioButton(
-                                                selected = (text == selectedOption),
-                                                modifier = Modifier.padding(all = Dp(value = 8F)),
-                                                onClick = {
-                                                    onOptionSelected(text)
-                                                    Toast.makeText(context, text, Toast.LENGTH_LONG).show()
-                                                }
-                                            )
-                                        }
-
+                                RadioButton(
+                                    selected = (text == selectedOption),
+                                    modifier = Modifier.padding(all = Dp(value = 8F)),
+                                    onClick = {
+                                        onOptionSelected(text)
+                                        Toast.makeText(context, text, Toast.LENGTH_LONG).show()
                                     }
-
-                                }
-                                Spacer(modifier= Modifier.height(10.dp))
-
+                                )
                             }
+
                         }
+
                     }
+                    Spacer(modifier= Modifier.height(10.dp))
 
                 }
+
+                Spacer(modifier=Modifier.height(100.dp))
                 Spacer(modifier=Modifier
                     .fillMaxWidth()
                     .height(1.dp)
                     .background(color=Color.Black))
+                Divider()
                 Spacer(modifier=Modifier.height(5.dp))
+
+                Button(
+                    onClick={ navController.navigate(NavigationItem.LogInPromptScreen.route) },
+                    colors=ButtonDefaults.buttonColors(
+                        backgroundColor=colorResource(id=R.color.slimmyGreen),
+                        contentColor=Color.Black,
+                    ),
+                    modifier=Modifier.fillMaxWidth(),
+                    shape=RoundedCornerShape(23.dp),
+                    border=BorderStroke(3.dp, Color.White),
+                ) {
+                Text(text= "DONE")
+
+            }
+
+
 
             }
         }
-        Spacer(modifier=Modifier.height(5.dp))
 
 
-           Button(
-               onClick={ navController.navigate(NavigationItem.LogInPromptScreen.route) },
-               colors=ButtonDefaults.buttonColors(
-                   backgroundColor=Color.Transparent,
-                   contentColor=Color.Black,
-               ),
-           ) {
-               Text(text="DONE")
 
-           }
 
-           Spacer(modifier=Modifier.height(30.dp))
        }
+}
 
 
 
